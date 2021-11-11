2b73024eb123a74c9c3e92b26a83a3ae9abf270d
Nikita Konopelko <nik.sativa@gmail.com>
2021-11-11 23:27:31 +0200

    GHA experiments

--- Package.swift
+++ Package.swift
@@ -1,17 +1,17 @@
-// swift-tools-version:5.5
+// swift-tools-version:5.2
 
 import PackageDescription
 
 // swiftformat:disable all
 let package = Package(
     name: "NQueue",
-    platforms: [.iOS(.v10)],
+    platforms: [.iOS(.v10), .macOS(.v10_12)],
     products: [
         .library(name: "NQueue", targets: ["NQueue"]),
         .library(name: "NQueueTestHelpers", targets: ["NQueueTestHelpers"])
     ],
     dependencies: [
-        .package(url: "https://github.com/NikSativa/NSpry.git", .upToNextMajor(from: "1.1.2")),
+        .package(url: "https://github.com/NikSativa/NSpry.git", .upToNextMajor(from: "1.1.3")),
         .package(url: "https://github.com/Quick/Quick.git", .upToNextMajor(from: "4.0.0")),
         .package(url: "https://github.com/Quick/Nimble.git", .upToNextMajor(from: "9.2.0"))
     ],
