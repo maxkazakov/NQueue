3f4dc3fee1e6c91e0ebb18976c987d5e4e457a4d
Max Kazakov <kazakov.maixm.a@gmail.com>
2021-11-15 18:37:56 +0500

    InfoPlists + Header files

---
+++ NQueue-Carthage/NQueue/NQueue.h
@@ -0,0 +1,18 @@
+//
+//  NQueue.h
+//  NQueue
+//
+//  Created by Максим Казаков on 15.11.2021.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for NQueue.
+FOUNDATION_EXPORT double NQueueVersionNumber;
+
+//! Project version string for NQueue.
+FOUNDATION_EXPORT const unsigned char NQueueVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NQueue/PublicHeader.h>
+
+
---
+++ NQueue-Carthage/NQueueTestHelpers/NQueueTestHelpers.h
@@ -0,0 +1,18 @@
+//
+//  NQueueTestHelpers.h
+//  NQueueTestHelpers
+//
+//  Created by Максим Казаков on 15.11.2021.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for NQueueTestHelpers.
+FOUNDATION_EXPORT double NQueueTestHelpersVersionNumber;
+
+//! Project version string for NQueueTestHelpers.
+FOUNDATION_EXPORT const unsigned char NQueueTestHelpersVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NQueueTestHelpers/PublicHeader.h>
+
+
